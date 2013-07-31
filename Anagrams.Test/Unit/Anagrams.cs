5c32c972a1129a0457d4133619c069cc3742a308
alessioperry <ale.pam@gmail.com>
2013-07-31 10:29:07 +0200

    commit
    first commit

---
+++ Anagrams.Test/Unit/Anagrams.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace Anagrams.Test.Unit
+{
+    [TestFixture]
+    public class Anagrams
+    {
+        [Test]
+        public void AnagramOfAIsA()
+        {
+            var result = new Word().Anagram("a");
+
+            Assert.AreEqual("a", result);
+        }
+
+        [Test]
+        public void AnagramOfABIsABBA()
+        {
+            var result = new Word().Anagram("ab");
+
+            Assert.AreEqual("abba", result);
+        }
+
+        public class Word
+        {
+            public string Anagram(string word, int startIndex)
+            {
+                if (word.Length == 1)
+                    return word;
+
+                foreach (var chars in word)
+                {
+                    
+                }
+               
+                
+            }
+        }
+    }
+}
